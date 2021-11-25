414c746f8cdca504285f113a561ce58567766448
Enzo-Vargas83 <enzo.vargas@etu.univ-amu.fr>
2021-11-25 11:25:59 +0100

    affichage date fr et eng

--- Fonc_Start_n_EndPage.php
+++ Fonc_Start_n_EndPage.php
@@ -26,8 +26,8 @@
 
 <?php
     //echo date('l F d, Y');
-    $jourEng = date('F d, Y');
-    $jourFr = date('d/m/Y', strtotime('2020-04-01'));
+    $jourEng = date('F d, Y, H:i:s');
+    $jourFr = date('d/m/Y', strtotime('2021-11-25'));
     echo "$jourFr </br>";
     echo $jourEng;
 
