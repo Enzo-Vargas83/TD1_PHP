f4c490a05e53e832e83b54e022dfb90bcf5274b8
Enzo-Vargas83 <enzo.vargas@etu.univ-amu.fr>
2021-11-25 09:56:13 +0100

    premiere page php update

--- mapremierepage.php
+++ mapremierepage.php
@@ -13,8 +13,8 @@ $var1 = 6;
 $var2 = 1.3;
 $var3 = 'Variable 3';
 
-echo "$var1 + $var2";
-echo $var1 + $var2;
+echo "$var1 + $var2 = "; //affiche seulement le calcul
+echo $var1 + $var2; //effectue le cacul et l'affiche
 ?>
 </body>
 </html>
