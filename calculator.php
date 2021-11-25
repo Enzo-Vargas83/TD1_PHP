128910ddaa1183040f50a893ab9b4ab5257599e2
Enzo-Vargas83 <enzo.vargas@etu.univ-amu.fr>
2021-11-25 11:54:11 +0100

    ajout d'un form

--- calculator.php
+++ calculator.php
@@ -3,5 +3,21 @@
 ?>
 
 <?php
-   start_page('calculator');
+ start_page("calculator");
+?>
+<form action="calculator.php" method="post">
+    <label>
+        <input type="text" name="op1">
+        <input type="text" name="op2">
+        <input checked="checked" type="radio" name="op" value="*"/>*<br/>
+        <input type="radio" name="op" value="+"/>+<br/>
+        <input type="radio" name="op" value="-"/>-<br/>
+        <input type="radio" name="op" value="/"/>/<br/>
+        <input type="submit" name="submit">
+    </label>
+
+</form>
+
+<?php
+  end_page();
 ?>
