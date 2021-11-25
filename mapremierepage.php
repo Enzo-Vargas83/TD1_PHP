c1ca082c9c57215c9291cfaea7e975553c60f55d
Enzo-Vargas83 <enzo.vargas@etu.univ-amu.fr>
2021-11-25 09:21:14 +0100

    premiere page php

---
+++ mapremierepage.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <title>Title</title>
+</head>
+<body>
+<?php
+echo '<strong> Voici mon premier programme PHP </strong><br/>' . "\n";
+echo 'C\'est pas mal non ? <br/>';
+
+$var1 = 6;
+$var2 = 1.3;
+$var3 = 'Variable 3';
+
+echo "$var1 + $var2";
+echo $var1 + $var2;
+?>
+</body>
+</html>
