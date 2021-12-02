e2cec6d4cc162cd829333d9be6a513a545d6c2ad
Vargas Enzo <enzo.vargas@etu.univ-amu.fr>
2021-12-02 14:11:34 +0100

    changement action form

--- Fonc_Start_n_EndPage.php
+++ Fonc_Start_n_EndPage.php
@@ -19,11 +19,6 @@
     }
 ?>
 <?php
-    start_page('Test');
-    end_page();
-?>
-
-<?php
     //echo date('l F d, Y');
     $jourEng = date('F d, Y, H:i:s');
     $jourFr = date('d/m/Y', strtotime('2021-11-25'));
