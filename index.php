13808844ad4caeb5c39d53e32d2f3a7b2d84bfbe
Vargas Enzo <enzo.vargas@etu.univ-amu.fr>
2021-12-02 14:18:23 +0100

    changement action form

--- index.php
+++ index.php
@@ -1,23 +1,29 @@
+<?php include "start_end.php"?>
 <?php
-    include "Fonc_Start_n_EndPage.php";
+start_page("Calculator");
 ?>
-
 <?php
- start_page("calculator");
+$operateurs = '*+-/';
 ?>
-<form action="calcul.php" method="post">
-    <label>
-        <input type="text" name="op1">
-        <input type="text" name="op2"><br/>
-        <input checked="checked" type="radio" name="op" value="*"/>*<br/>
-        <input type="radio" name="op" value="+"/>+<br/>
-        <input type="radio" name="op" value="-"/>-<br/>
-        <input type="radio" name="op" value="/"/>/<br/>
-        <input type="submit" name="submit">
-    </label>
-
-</form>
-
+    <form action="calcul.php" method="post">
+        <label>
+            <input type="text" name="op2"><br>
+            <input type="text" name="op1"><br>
+            <?php
+            for($cpt = 0 ; $cpt <= 3 ; ++$cpt)
+            {
+                echo '<input ';
+                if($cpt == 0)
+                { echo 'checked="checked" '; }
+                echo 'type="radio" name="op" value="' . $operateurs[$cpt] . '"/>' . $operateurs[$cpt] . ' <br/>' . "\n";
+            }
+            ?>
+            <!--        <input checked="checked" type="radio" name="op" value="*"/>*<br/>-->
+            <!--        <input type="radio" name="op" value="+"/>+<br/>-->
+            <!--        <input type="radio" name="op" value="-"/>-<br/>-->
+            <!--        <input type="radio" name="op" value="/"/>/<br/>-->
+        </label>
+        <input type="submit" name="action">
+    </form>
 <?php
-  end_page();
-?>
+end_page();
